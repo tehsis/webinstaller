07ac742ef0969d04c5cf0edba10088c1f86e4b3a
Pablo Terradillos <tehsis@gmail.com>
2014-10-25 00:49:39 -0300

    fixed identation

--- webinstaller.js
+++ webinstaller.js
@@ -1,68 +1,68 @@
 (function() {
-	var WebInstaller = function(options) {
-		var self = this;
-		self.manifest = options.manifest || options;
-		self.classes = options.classes || {
-			installed: "installed",
-			not_installed: "not-installed",
-            not_installable: "not-installable",
-			error: "install-error"
-		};
+  var WebInstaller = function(options) {
+    var self = this;
+    self.manifest = options.manifest || options;
+    self.classes = options.classes || {
+      installed: "installed",
+      not_installed: "not-installed",
+      not_installable: "not-installable",
+      error: "install-error"
+    };
 
-		self.parent = options.parent || document.body;
+    self.parent = options.parent || document.body;
 
-		if (navigator.mozApps) {
-			var request = navigator.mozApps.checkInstalled(self.manifest);
-			request.onsuccess = function() {
-				if (request.result) {
-					self.parent.classList.add(self.classes.installed);
-				} else {
-					self.parent.classList.add(self.classes.not_installed);
-				}
-			};
-
-			request.onerror = function() {
-				self.parent.classList.add(self.classes.error);
-			};
-		} else {
-            self.parent.classList.add(self.classes.not_installable);
+    if (navigator.mozApps) {
+      var request = navigator.mozApps.checkInstalled(self.manifest);
+      request.onsuccess = function() {
+        if (request.result) {
+          self.parent.classList.add(self.classes.installed);
+        } else {
+          self.parent.classList.add(self.classes.not_installed);
         }
-	};
+      };
 
-	WebInstaller.prototype.install = function() {
-		var self = this;
-		if (navigator.mozApps.checkInstalled(self.manifest)) { 
-			var request = navigator.mozApps.getSelf();
+      request.onerror = function() {
+        self.parent.classList.add(self.classes.error);
+      };
+    } else {
+      self.parent.classList.add(self.classes.not_installable);
+    }
+  };
 
-			request.onsuccess = function() {
-				if (!request.result) {
-                    var install_request = navigator.mozApps.install(self.manifest);
+  WebInstaller.prototype.install = function() {
+    var self = this;
+    if (navigator.mozApps.checkInstalled(self.manifest)) { 
+      var request = navigator.mozApps.getSelf();
 
-					// Apparently this is failing on Android and desktop due: https://bugzilla.mozilla.org/show_bug.cgi?id=806597
-					install_request.onsuccess = function() {
-						self.parent.classList.remove(self.classes.not_installed);
-						self.parent.classList.add(self.classes.installed);
-                    };
+      request.onsuccess = function() {
+        if (!request.result) {
+          var install_request = navigator.mozApps.install(self.manifest);
 
-					install_request.onerror = function() {
-						self.parent.classList.remove(self.classes.not_installed);
-						self.parent.classList.add(self.classes.error);
-                        console.log(this.error.name);
-					};
-				}
-			};
+          // Apparently this is failing on Android and desktop due: https://bugzilla.mozilla.org/show_bug.cgi?id=806597
+          install_request.onsuccess = function() {
+            self.parent.classList.remove(self.classes.not_installed);
+            self.parent.classList.add(self.classes.installed);
+          };
 
-			request.onerror = function() {
-				self.parent.classList.add(self.classes.error);
-			};
-		}
-	};
+          install_request.onerror = function() {
+            self.parent.classList.remove(self.classes.not_installed);
+            self.parent.classList.add(self.classes.error);
+            console.log(this.error.name);
+          };
+        }
+      };
+
+      request.onerror = function() {
+        self.parent.classList.add(self.classes.error);
+      };
+    }
+  };
 
 
-	if ( typeof define === "function" && define.amd ) {
-        define([], function () { return WebInstaller; } );
-	} else {
-		window.WebInstaller = window.WebInstaller || WebInstaller;
-	}
+  if ( typeof window.define === "function" && window.define.amd ) {
+    window.define([], function () { return WebInstaller; } );
+  } else {
+    window.WebInstaller = window.WebInstaller || WebInstaller;
+  }
 
 }) ();
